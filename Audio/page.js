43bda188162f8e03b01f9495efcac0f81b7533cc
Spark-unknown <priyanshurathod.22.cse@iite.indusuni.ac.in>
2024-10-11 14:50:54 +0530

    Add files via upload

---
+++ Audio/page.js
@@ -0,0 +1,31 @@
+const songDataset = [
+    {
+      id: 1,
+      title: "Song One",
+      artist: "Artist One",
+      album: "Album One",
+      duration: "3:45",
+      url: "/path-to-song-1.mp3",
+      image: "/path-to-album-cover-1.jpg",
+    },
+    {
+      id: 2,
+      title: "Song Two",
+      artist: "Artist Two",
+      album: "Album Two",
+      duration: "4:20",
+      url: "/path-to-song-2.mp3",
+      image: "/path-to-album-cover-2.jpg",
+    },
+    {
+      id: 3,
+      title: "Song Three",
+      artist: "Artist Three",
+      album: "Album Three",
+      duration: "2:58",
+      url: "/path-to-song-3.mp3",
+      image: "/path-to-album-cover-3.jpg",
+    },
+    // Add more songs here
+  ];
+  
